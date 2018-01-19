8f41029fb3ef73fd7b41d328c12c5870476e2d38
Pat O'Callaghan <patocallaghan@gmail.com>
2018-01-19 11:50:31 +0000

    Added broccoli-persistent-filter

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 /* eslint-env node */
 'use strict';
 
+var BroccoliDebug = require('broccoli-debug');
 var TestTransformFilter = require('./lib/tests-transform-filter');
 
 module.exports = {
   name: 'ember-qunit-nice-errors',
+
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+    this.debugTree = BroccoliDebug.buildDebugCallback('ember-qunit-nice-errors');
+  },
 
   included: function(app) {
     this._super.included.apply(this, arguments);
@@ -15,7 +21,10 @@ module.exports = {
 
   preprocessTree: function(type, tree) {
     if (type === 'test') {
-      return new TestTransformFilter(tree, this.addonConfig);
+      var inputTree = this.debugTree(tree, 'input');
+      var processedTree = new TestTransformFilter(tree, this.addonConfig);
+      var outputTree = this.debugTree(processedTree, 'output');
+      return processedTree;
     }
     return tree;
   }
